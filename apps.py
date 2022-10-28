89f3149a5516f587123840fb3eb4b5684a21a47b
Laurakey <114479088+Laurakey@users.noreply.github.com>
2022-10-28 07:01:21 -1200

    Add files via upload

---
+++ apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MusiboxConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Musibox'
---
+++ models.py
@@ -0,0 +1,30 @@
+from email.policy import default
+from unittest.util import _MAX_LENGTH
+from django.db import models
+from datetime import datetime
+from django.conf import settings
+
+# Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length = 40)
+    last_name = models.CharField(max_length = 40)
+    age = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return f'{self.first_name}{self.last_name}'
+
+class Song(models.Model):
+    title = models.CharField(max_length = 40)
+    date_released = models.DateField(auto_now_add = False)
+    likes = models.IntegerField(default = 0)
+    artiste_id = models.ForeignKey(Artiste, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Lyric(models.Model):
+    content = models.TextField()
+    song_id = models.ForeignKey(Song, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.content
